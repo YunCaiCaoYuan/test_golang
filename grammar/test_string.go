45ff9074934e3ec128c4f86af355db04cef6a5bd
sunbin <769460962@qq.com>
2021-04-22 14:17:09 +0800

    test race

--- grammar/test_string.go
+++ grammar/test_string.go
@@ -1,17 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 func main() {
-	ret := strings.Contains("新手宝宝注意查收一下", "100")
-	fmt.Println(ret)
+	i := small(10)
+	println("i=", i)
 
+
+	/*
+	ret := strings.Contains("新手宝宝注意查收一下", "100")
+	fmt.Println(ret)*/
 	/*
 		str := " 123 "
 		ret := strings.TrimSpace(str)
 		fmt.Println(ret)
 	*/
+}
+
+const digits = "0123456789abcdefghijklmnopqrstuvwxyz"
+const smallsString = "00010203040506070809" +
+	"10111213141516171819" +
+	"20212223242526272829" +
+	"30313233343536373839" +
+	"40414243444546474849" +
+	"50515253545556575859" +
+	"60616263646566676869" +
+	"70717273747576777879" +
+	"80818283848586878889" +
+	"90919293949596979899"
+func small(i int) string {
+	if i < 10 {
+		return digits[i : i+1]
+	}
+	return smallsString[i*2 : i*2+2]
 }
