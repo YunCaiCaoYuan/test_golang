0f81fcbf24daa691639258a183d3152011524e8f
sunbin <769460962@qq.com>
2021-04-27 14:13:08 +0800

    test location

--- library/test_time.go
+++ library/test_time.go
@@ -20,6 +20,17 @@ func main() {
 	 */
 
 	/*
+	Time, _ := time.Parse("2006-01-02 15:04:05", "2021-03-23 00:08:00")
+	fmt.Println("Time=", Time)
+	fmt.Println("Now=", time.Now())
+	 */
+
+	loc, _ := time.LoadLocation("Europe/Berlin")
+	const longForm = "Jan 2, 2006 at 3:04pm (MST)"
+	t, _ := time.ParseInLocation(longForm, "Jul 9, 2012 at 5:02am (CEST)", loc)
+	fmt.Println(t)
+
+	/*
 	now := time.Now().Unix()
 	fmt.Println("now=", now)
 	 */
@@ -29,8 +40,10 @@ func main() {
 	fmt.Println("now=", now) //  1619320725105078000
 	 */
 
+	/*
 	nowStr := fmt.Sprintf("%d", time.Now().UnixNano())
 	fmt.Println("nowStr=", nowStr) //  1619320725105078000
+	 */
 
 	/*
 	timeUsed := time.Duration(148104536007)
