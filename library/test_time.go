66f0fa873f45cf8cfde17fd101475b9086a55288
sunbin <769460962@qq.com>
2021-04-30 17:09:41 +0800

    time zone

--- library/test_time.go
+++ library/test_time.go
@@ -19,16 +19,21 @@ func main() {
 	println("uint64 timeAdd=", timeAdd)
 	 */
 
-	/*
-	Time, _ := time.Parse("2006-01-02 15:04:05", "2021-03-23 00:08:00")
-	fmt.Println("Time=", Time)
-	fmt.Println("Now=", time.Now())
-	 */
 
+
+	Time, _ := time.Parse("2006-01-02 15:04:05", "2021-03-23 08:00:00")
+	fmt.Println("Time1=", Time)
+	//fmt.Println("Now=", time.Now())
+	Time, _ = time.ParseInLocation("2006-01-02 15:04:05", "2021-03-23 08:00:00", time.Local)
+	fmt.Println("Time2=", Time)
+
+
+	/*
 	loc, _ := time.LoadLocation("Europe/Berlin")
 	const longForm = "Jan 2, 2006 at 3:04pm (MST)"
 	t, _ := time.ParseInLocation(longForm, "Jul 9, 2012 at 5:02am (CEST)", loc)
 	fmt.Println(t)
+	 */
 
 	/*
 	now := time.Now().Unix()
