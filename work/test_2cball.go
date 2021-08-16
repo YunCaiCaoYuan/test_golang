4c66232772d192c287f357b5bea23281f306ef43
sunb <769460962@qq.com>
2021-08-16 22:05:07 +0800

    2 c ball

--- work/test_2cball.go
+++ work/test_2cball.go
@@ -27,6 +27,7 @@ func twoCBall(rList, rList1 sort.IntSlice, blue, blue1 int) {
 	}
 	//fmt.Println("heartWaterMap=", heartWaterMap)
 	fmt.Println("killRedMap1=", killRedMap1)
+
 	killRedMap2 := make(map[int]bool)
 	for i := len(rList) - 1; i >= 0; i-- {
 		killRed := rList[i] - rList1[i]
@@ -38,6 +39,7 @@ func twoCBall(rList, rList1 sort.IntSlice, blue, blue1 int) {
 		}
 	}
 	fmt.Println("killRedMap2=", killRedMap2)
+
 	killRedMap3 := make(map[int]bool)
 	for i := len(rList) - 1; i >= 0; i-- {
 		killRed := rList[i] - blue1
@@ -49,6 +51,68 @@ func twoCBall(rList, rList1 sort.IntSlice, blue, blue1 int) {
 		}
 	}
 	fmt.Println("killRedMap3=", killRedMap3)
+
+	ACVal := len(killRedMap1) - (len(rList) - 1)
+	killRedMap4 := make(map[int]bool)
+	for i := 0; i < len(rList); i++ {
+		killRed := rList[i] - ACVal
+		if killRed < 0 {
+			killRed = 33 + killRed
+		}
+		if !killRedMap4[killRed] {
+			killRedMap4[killRed] = true
+		}
+	}
+	fmt.Println("killRedMap4=", killRedMap4)
+
+	killRedMap5 := make(map[int]bool)
+	total := 0
+	for i := 0; i < len(rList); i++ {
+		total += rList[i]
+	}
+	divSum := calcDivSum(total)
+	for i := 0; i < len(rList); i++ {
+		killRed := rList[i] - divSum
+		if killRed < 0 {
+			killRed = 33 + killRed
+		}
+		if !killRedMap5[killRed] {
+			killRedMap5[killRed] = true
+		}
+	}
+	fmt.Println("killRedMap5=", killRedMap5)
+
+	killRedMap6 := make(map[int]bool)
+	tailTotal := 0
+	for i := 0; i < len(rList); i++ {
+		tailTotal += rList[i] - (rList[i]/10)*10
+	}
+	tailSum := calcDivSum(tailTotal)
+	for i := 0; i < len(rList); i++ {
+		killRed := rList[i] - tailSum
+		if killRed < 0 {
+			killRed = 33 + killRed
+		}
+		if !killRedMap6[killRed] {
+			killRedMap6[killRed] = true
+		}
+	}
+	fmt.Println("killRedMap6=", killRedMap6)
+}
+
+func calcDivSum(total int) int {
+	divSum := 0
+	for {
+		if total > 10 {
+			shangVal := total / 10
+			divSum += total - shangVal*10
+			total = shangVal
+		} else {
+			divSum += total
+			break
+		}
+	}
+	return divSum
 }
 
 func main() {
