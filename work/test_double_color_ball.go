201cbe78cf822122d86766c7a47f3af18c7d2e64
sunb <769460962@qq.com>
2021-08-16 22:55:55 +0800

    总和和大小

--- work/test_double_color_ball.go
+++ work/test_double_color_ball.go
@@ -8,9 +8,9 @@ import (
 )
 
 const (
-	redMax = 33
+	redMax  = 33
 	buleMax = 16
-	redNum = 6
+	redNum  = 6
 	buleNum = 1
 )
 const Cnt = 5
@@ -18,7 +18,7 @@ const Cnt = 5
 func randNum(max, cnt int) sort.IntSlice {
 	numMap := make(map[int]bool)
 	for {
-		num := rand.Intn(max)+1
+		num := rand.Intn(max) + 1
 		if !numMap[num] {
 			numMap[num] = true
 		}
@@ -27,18 +27,40 @@ func randNum(max, cnt int) sort.IntSlice {
 		}
 	}
 	list := make(sort.IntSlice, 0, max)
-	for num,_ := range numMap {
+	for num, _ := range numMap {
 		list = append(list, num)
 	}
 	list.Sort()
 	return list
 }
 
+func wrap(num int) string {
+	return fmt.Sprintf("%02d", num)
+}
+
 func main() {
 	rand.Seed(time.Now().Unix())
-	for i:=0; i<Cnt; i++ {
+	cnt := 0
+	for {
+		total := 0
+		bigNum := 0
 		list1 := randNum(redMax, redNum)
-		list2 := randNum(buleMax, buleNum)
-		fmt.Println(list1[0], list1[1], list1[2], list1[3], list1[4], list1[5], list2[0])
+		for i := 0; i < len(list1); i++ {
+			total += list1[i]
+			if list1[i] >= 18 {
+				bigNum++
+			}
+		}
+		if total >= 88 && total <= 117 {
+			// 去除大小15或06
+			if bigNum <= 1 || bigNum >= 5 {
+				continue
+			}
+			cnt += 1
+			fmt.Println(wrap(list1[0]), wrap(list1[1]), wrap(list1[2]), wrap(list1[3]), wrap(list1[4]), wrap(list1[5]))
+		}
+		if cnt >= 16 {
+			break
+		}
 	}
 }
