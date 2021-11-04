dac7e3f90d4dfc2b68b4cef88101521e4b4f9cbd
sunbin <769460962@qq.com>
2021-11-04 15:47:24 +0800

    tetris

--- library/test_rand.go
+++ library/test_rand.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().Unix()) // 初始化随机种子
 	for i := 0; i<10; i++ {
-		r := rand.Int()
+		r := rand.Intn(10)
 		fmt.Println(r)
 	}
-
 }
