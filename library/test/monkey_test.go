c4d3a072764ca26a3bef1b2a39bfef39bc983ea5
sunbin <769460962@qq.com>
2021-11-12 18:19:40 +0800

    monkey

--- library/test/monkey_test.go
+++ library/test/monkey_test.go
@@ -3,41 +3,52 @@ package test
 import (
 	. "bou.ke/monkey"
 	"fmt"
-	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
 
 func IsEqual(a, b string) bool {
 	fmt.Println("not monkey")
+	fmt.Println("not monkey")
+	fmt.Println("not monkey")
+	fmt.Println("not monkey")
+	fmt.Println("not monkey")
+	fmt.Println("not monkey")
+	fmt.Println("not monkey")
+	fmt.Println("not monkey")
+	fmt.Println("not monkey")
+	fmt.Println("not monkey")
+	fmt.Println("not monkey")
+	fmt.Println("not monkey")
 	return a == b
 }
 
 func TestIsEqual(t *testing.T) {
-	Convey("test is equal", t, func() {
-		Convey("for patch true", func() {
 			guard := Patch(IsEqual, func(_, _ string) bool {
 				fmt.Println("monkey...")
 				return true
 			})
 			defer guard.Unpatch()
 			ok := IsEqual("hello", "world")
-			So(ok, ShouldBeTrue)
-		})
-	})
+			if ok {
+				fmt.Println("ok")
+			} else {
+				fmt.Println("not ok")
+			}
 }
 
-/*
-func TestExec(t *testing.T) {
-	Convey("test has digit", t, func() {
-		Convey("for succ", func() {
-			outputExpect := "xxx-vethName100-yyy"
-			guard := Patch(osencap.Exec, func(_ string, _ ...string) (string, error) {
-				return outputExpect, nil
-			})
-			defer guard.Unpatch()
-			output, err := osencap.Exec(any, any)
-			So(output, ShouldEqual, outputExpect)
-			So(err, ShouldBeNil)
-		})
-	})
-}*/
+// 编译器内联掉了，把函数改复杂一些，就不会被内联了
+//[root@sunbinaliyun tmp]# go test -v monkey_test.go
+//=== RUN   TestIsEqual
+//not monkey
+//not ok
+//--- PASS: TestIsEqual (0.00s)
+//PASS
+//ok  	command-line-arguments	0.002s
+
+//[root@sunbinaliyun tmp]# go test -gcflags "all=-N -l" -v monkey_test.go
+//=== RUN   TestIsEqual
+//monkey...
+//ok
+//--- PASS: TestIsEqual (0.00s)
+//PASS
+//ok  	command-line-arguments	0.002s
--- library/test_rand.go
+++ library/test_rand.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
 func main() {
-	rand.Seed(time.Now().Unix()) // 初始化随机种子
+	rand.Seed(1234567890) // 初始化随机种子
 	for i := 0; i<10; i++ {
 		r := rand.Intn(10)
 		fmt.Println(r)
