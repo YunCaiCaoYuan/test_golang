8c47c7f1e5775955eeba360dfb471e8f1c23bb98
sunbin <769460962@qq.com>
2021-11-18 14:13:21 +0800

    smtp(qq邮箱）

--- go.mod
+++ go.mod
@@ -8,10 +8,12 @@ require (
 	github.com/agiledragon/gomonkey/v2 v2.2.0
 	github.com/aliyun/aliyun-oss-go-sdk v2.1.6+incompatible
 	github.com/baiyubin/aliyun-sts-go-sdk v0.0.0-20180326062324-cfa1a18b161f // indirect
+	github.com/go-gomail/gomail v0.0.0-20160411212932-81ebce5c23df
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/goburrow/cache v0.1.3
 	github.com/golang/protobuf v1.4.2
 	github.com/jinzhu/gorm v1.9.16
+	github.com/matcornic/hermes/v2 v2.1.0
 	github.com/onsi/ginkgo v1.16.4
 	github.com/onsi/gomega v1.10.5
 	github.com/prashantv/gostub v1.0.0
@@ -24,6 +26,7 @@ require (
 	github.com/stretchr/testify v1.5.1
 	github.com/vmihailenco/msgpack v3.3.3+incompatible
 	go.uber.org/zap v1.16.0
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	golang.org/x/text v0.3.3
---
+++ work/smtp/test_smtp.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/go-gomail/gomail"
+	"github.com/matcornic/hermes/v2"
+	"golang.org/x/crypto/ssh/terminal"
+	"io/ioutil"
+	"net/mail"
+	"os"
+	"strconv"
+)
+
+//HERMES_SEND_EMAILS=true;HERMES_SMTP_PORT=587;HERMES_SMTP_SERVER=smtp.qq.com;HERMES_SMTP_USER=769460962;HERMES_SMTP_PASSWORD=mbaxtfukgubzbbfc;HERMES_SENDER_IDENTITY=769460962;HERMES_SENDER_EMAIL=769460962@qq.com;HERMES_TO=769460962@qq.com
+
+type example interface {
+	Email() hermes.Email
+	Name() string
+}
+
+func main() {
+
+	h := hermes.Hermes{
+		Product: hermes.Product{
+			Name: "Hermes",
+			Link: "https://example-hermes.com/",
+			Logo: "https://github.com/matcornic/hermes/blob/master/examples/gopher.png?raw=true",
+		},
+	}
+	sendEmails := os.Getenv("HERMES_SEND_EMAILS") == "true"
+
+	examples := []example{
+		//new(welcome),
+		//new(reset),
+		//new(receipt),
+		//new(maintenance),
+		new(inviteCode),
+	}
+
+	themes := []hermes.Theme{
+		new(hermes.Default),
+		//new(hermes.Flat),
+	}
+
+	// Generate emails
+
+	for _, theme := range themes {
+		h.Theme = theme
+		for _, e := range examples {
+			generateEmails(h, e.Email(), e.Name())
+		}
+	}
+
+	// Send emails only when requested
+	if sendEmails {
+		port, _ := strconv.Atoi(os.Getenv("HERMES_SMTP_PORT"))
+		password := os.Getenv("HERMES_SMTP_PASSWORD")
+		SMTPUser := os.Getenv("HERMES_SMTP_USER")
+		if password == "" {
+			fmt.Printf("Enter SMTP password of '%s' account: ", SMTPUser)
+			bytePassword, _ := terminal.ReadPassword(0)
+			password = string(bytePassword)
+		}
+		smtpConfig := smtpAuthentication{
+			Server:         os.Getenv("HERMES_SMTP_SERVER"),
+			Port:           port,
+			SenderEmail:    os.Getenv("HERMES_SENDER_EMAIL"),
+			SenderIdentity: os.Getenv("HERMES_SENDER_IDENTITY"),
+			SMTPPassword:   password,
+			SMTPUser:       SMTPUser,
+		}
+		options := sendOptions{
+			To: os.Getenv("HERMES_TO"),
+		}
+		for _, theme := range themes {
+			h.Theme = theme
+			for _, e := range examples {
+				options.Subject = "Hermes | " + h.Theme.Name() + " | " + e.Name()
+				fmt.Printf("Sending email '%s'...\n", options.Subject)
+				htmlBytes, err := ioutil.ReadFile(fmt.Sprintf("%v/%v.%v.html", h.Theme.Name(), h.Theme.Name(), e.Name()))
+				if err != nil {
+					panic(err)
+				}
+				txtBytes, err := ioutil.ReadFile(fmt.Sprintf("%v/%v.%v.txt", h.Theme.Name(), h.Theme.Name(), e.Name()))
+				if err != nil {
+					panic(err)
+				}
+				err = send(smtpConfig, options, string(htmlBytes), string(txtBytes))
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+}
+
+func generateEmails(h hermes.Hermes, email hermes.Email, example string) {
+	// Generate the HTML template and save it
+	res, err := h.GenerateHTML(email)
+	if err != nil {
+		panic(err)
+	}
+	err = os.MkdirAll(h.Theme.Name(), 0744)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(fmt.Sprintf("%v/%v.%v.html", h.Theme.Name(), h.Theme.Name(), example), []byte(res), 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	// Generate the plaintext template and save it
+	res, err = h.GeneratePlainText(email)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(fmt.Sprintf("%v/%v.%v.txt", h.Theme.Name(), h.Theme.Name(), example), []byte(res), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type smtpAuthentication struct {
+	Server         string
+	Port           int
+	SenderEmail    string
+	SenderIdentity string
+	SMTPUser       string
+	SMTPPassword   string
+}
+
+// sendOptions are options for sending an email
+type sendOptions struct {
+	To      string
+	Subject string
+}
+
+// send sends the email
+func send(smtpConfig smtpAuthentication, options sendOptions, htmlBody string, txtBody string) error {
+
+	if smtpConfig.Server == "" {
+		return errors.New("SMTP server config is empty")
+	}
+	if smtpConfig.Port == 0 {
+		return errors.New("SMTP port config is empty")
+	}
+
+	if smtpConfig.SMTPUser == "" {
+		return errors.New("SMTP user is empty")
+	}
+
+	if smtpConfig.SenderIdentity == "" {
+		return errors.New("SMTP sender identity is empty")
+	}
+
+	if smtpConfig.SenderEmail == "" {
+		return errors.New("SMTP sender email is empty")
+	}
+
+	if options.To == "" {
+		return errors.New("no receiver emails configured")
+	}
+
+	from := mail.Address{
+		Name:    smtpConfig.SenderIdentity,
+		Address: smtpConfig.SenderEmail,
+	}
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", from.String())
+	m.SetHeader("To", options.To)
+	m.SetHeader("Subject", options.Subject)
+
+	m.SetBody("text/plain", txtBody)
+	m.AddAlternative("text/html", htmlBody)
+
+	d := gomail.NewDialer(smtpConfig.Server, smtpConfig.Port, smtpConfig.SMTPUser, smtpConfig.SMTPPassword)
+
+	return d.DialAndSend(m)
+}
