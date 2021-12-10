1eb7c9d51c76e5910e0fd9015eaac560546f5ecb
sunbin <769460962@qq.com>
2021-12-10 18:31:14 +0800

    资源服务器

---
+++ work/resource_server/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"resource_server/config"
+	"resource_server/endpoint"
+	"resource_server/service"
+	"resource_server/transport"
+	"strconv"
+	"syscall"
+)
+
+func main() {
+
+	var (
+		servicePort = flag.Int("service.port", 10099, "service port")
+	)
+
+	flag.Parse()
+
+	ctx := context.Background()
+	errChan := make(chan error)
+
+	var tokenService service.ResourceServerTokenService
+	var tokenEnhancer service.TokenEnhancer
+	var tokenStore service.TokenStore
+	var srv service.Service
+
+	tokenEnhancer = service.NewJwtTokenEnhancer("secret")
+	tokenStore = service.NewJwtTokenStore(tokenEnhancer.(*service.JwtTokenEnhancer))
+	tokenService = service.NewTokenService(tokenStore, tokenEnhancer)
+
+	srv = service.NewCommonService()
+
+	indexEndpoint := endpoint.MakeIndexEndpoint(srv)
+	sampleEndpoint := endpoint.MakeSampleEndpoint(srv)
+	sampleEndpoint = endpoint.MakeOAuth2AuthorizationMiddleware(config.KitLogger)(sampleEndpoint)
+	adminEndpoint := endpoint.MakeAdminEndpoint(srv)
+	adminEndpoint = endpoint.MakeOAuth2AuthorizationMiddleware(config.KitLogger)(adminEndpoint)
+	adminEndpoint = endpoint.MakeAuthorityAuthorizationMiddleware("Admin", config.KitLogger)(adminEndpoint)
+
+	//创建健康检查的Endpoint
+	healthEndpoint := endpoint.MakeHealthCheckEndpoint(srv)
+
+	endpts := endpoint.OAuth2Endpoints{
+		HealthCheckEndpoint: healthEndpoint,
+		IndexEndpoint:       indexEndpoint,
+		SampleEndpoint:      sampleEndpoint,
+		AdminEndpoint:       adminEndpoint,
+	}
+
+	//创建http.Handler
+	r := transport.MakeHttpHandler(ctx, endpts, tokenService, config.KitLogger)
+
+	go func() {
+		config.Logger.Println("Http Server start at port:" + strconv.Itoa(*servicePort))
+		handler := r
+		errChan <- http.ListenAndServe(":"+strconv.Itoa(*servicePort), handler)
+	}()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+
+	error := <-errChan
+	config.Logger.Println(error)
+}
