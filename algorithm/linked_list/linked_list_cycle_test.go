2705fc5b9d0db7ecaab6775f589e118c04134845
sunbin <>
2022-03-29 11:03:18 +0800

    环形链表II

--- algorithm/linked_list/linked_list_cycle_test.go
+++ algorithm/linked_list/linked_list_cycle_test.go
@@ -21,6 +21,44 @@ func hasCycle(head *Node) bool {
 	return false
 }
 
+// 快慢指针
+func detectCycle(head *Node) *Node {
+	show := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		show = show.Next
+		fast = fast.Next.Next
+		if show == fast { // 相遇即有环
+			for show != head {
+				show = show.Next
+				head = head.Next
+			}
+			return head
+		}
+	}
+	return nil
+}
+
+func detectCycle2(head *Node) *Node {
+	show := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		show = show.Next
+		fast = fast.Next.Next
+		if show == fast { // 相遇即有环
+			break
+		}
+	}
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+	for show != head {
+		show = show.Next
+		head = head.Next
+	}
+	return head
+}
+
 // 3->2->0->-4 pos=1
 func Test_hasCycle1(t *testing.T) {
 	node4 := &Node{Val: -4}
@@ -32,6 +70,8 @@ func Test_hasCycle1(t *testing.T) {
 	node3.Next = node4
 	node4.Next = node2
 	fmt.Println(hasCycle(node1))
+	fmt.Println(detectCycle(node1))
+	fmt.Println(detectCycle2(node1))
 }
 
 // 1->2 pos=0
@@ -41,12 +81,16 @@ func Test_hasCycle2(t *testing.T) {
 	node1.Next = node2
 	node2.Next = node1
 	fmt.Println(hasCycle(node1))
+	fmt.Println(detectCycle(node1))
+	fmt.Println(detectCycle2(node1))
 }
 
 // 1 pos=-1
 func Test_hasCycle3(t *testing.T) {
 	node1 := &Node{Val: 1}
 	fmt.Println(hasCycle(node1))
+	fmt.Println(detectCycle(node1))
+	fmt.Println(detectCycle2(node1))
 }
 
 // eg: go test -run=hasCycle
