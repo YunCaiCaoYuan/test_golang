4834243b533fb36aa28b7cd270a434e681d1b5d1
sunbin <>
2022-04-12 17:39:22 +0800

    test divide

--- grammar/test/common_test.go
+++ grammar/test/common_test.go
@@ -12,3 +12,10 @@ func Test_plus(t *testing.T) {
 	fmt.Println(a)
 	a++
 }
+
+func Test_divide(t *testing.T) {
+	a := 1
+	b := 2
+	fmt.Printf("%d\n", a/b)
+	fmt.Printf("%0.2f\n", float32(a)/float32(b))
+}
