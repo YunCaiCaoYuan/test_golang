786f6cfc48e893a8515572a2835e48ef54b90334
sunbin <>
2022-05-20 17:22:42 +0800

    test: float

--- grammar/test_int.go
+++ grammar/test_int.go
@@ -3,24 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("0 / 10 =", 0 / 10 * 10)
-	fmt.Println("9 / 10 =", 9 / 10 * 10)
-	fmt.Println("12 / 10 =", 12 / 10 * 10)
-	fmt.Println("20 / 10 =", 20 / 10 * 10)
+	fmt.Println("0 / 10 =", 0/10*10)
+	fmt.Println("9 / 10 =", 9/10*10)
+	fmt.Println("12 / 10 =", 12/10*10)
+	fmt.Println("20 / 10 =", 20/10*10)
+	fmt.Println("1 / 37 * 100 =", 1/37*100)
+	var f1 float32 = 1.0
+	fmt.Printf("1.0 / 37 * 100 =%6.2f\n", f1/37*100)
+	fmt.Printf("float32(1 / 37 * 100) =%6.2f\n", float32(1.0/37*100))
 
 	var intval int64
 	fmt.Println("intval=", intval)
 
 	/*
-	var i1 int = 1
-	var i2 int8 = 2
-	var i3 int16 = 3
-	var i4 int32 = 4
-	var i5 int64 = 5
-	fmt.Println(unsafe.Sizeof(i1)) // 8
-	fmt.Println(unsafe.Sizeof(i2)) // 1
-	fmt.Println(unsafe.Sizeof(i3)) // 2
-	fmt.Println(unsafe.Sizeof(i4)) // 4
-	fmt.Println(unsafe.Sizeof(i5)) // 8
-	 */
+		var i1 int = 1
+		var i2 int8 = 2
+		var i3 int16 = 3
+		var i4 int32 = 4
+		var i5 int64 = 5
+		fmt.Println(unsafe.Sizeof(i1)) // 8
+		fmt.Println(unsafe.Sizeof(i2)) // 1
+		fmt.Println(unsafe.Sizeof(i3)) // 2
+		fmt.Println(unsafe.Sizeof(i4)) // 4
+		fmt.Println(unsafe.Sizeof(i5)) // 8
+	*/
 }
