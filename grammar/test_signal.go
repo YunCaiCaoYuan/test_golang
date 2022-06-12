be1c85447935e99884f1a0f4e0220c8ff83fbfd0
sunb <769460962@qq.com>
2022-06-12 10:26:44 +0800

    signal

---
+++ grammar/test_signal.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	//创建监听退出chan
+	c := make(chan os.Signal)
+	//监听指定信号 ctrl+c kill
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM,
+		syscall.SIGQUIT, syscall.SIGUSR1, syscall.SIGUSR2)
+	go func() {
+		for s := range c {
+			switch s {
+			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+				fmt.Println("Program Exit...", s)
+				GracefullExit()
+			case syscall.SIGUSR1:
+				fmt.Println("usr1 signal", s)
+			case syscall.SIGUSR2:
+				fmt.Println("usr2 signal", s)
+			default:
+				fmt.Println("other signal", s)
+			}
+		}
+	}()
+
+	fmt.Println("Program Start...")
+	sum := 0
+	for {
+		sum++
+		fmt.Println("sum:", sum)
+		time.Sleep(time.Second)
+	}
+}
+
+func GracefullExit() {
+	fmt.Println("Start Exit...")
+	fmt.Println("Execute Clean...")
+	fmt.Println("End Exit...")
+	os.Exit(0)
+}
