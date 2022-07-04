0dda62a96f4ef7781cbb4fc144e76366c40040c2
sunbin <>
2022-07-04 15:19:45 +0800

    debug

--- work/test_es.go
+++ work/test_es.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/olivere/elastic"
 	"log"
@@ -8,6 +10,17 @@ import (
 	"os"
 	"time"
 )
+
+type Tweet struct {
+	User     string                `json:"user"`
+	Message  string                `json:"message"`
+	Retweets int                   `json:"retweets"`
+	Image    string                `json:"image,omitempty"`
+	Created  time.Time             `json:"created,omitempty"`
+	Tags     []string              `json:"tags,omitempty"`
+	Location string                `json:"location,omitempty"`
+	Suggest  *elastic.SuggestField `json:"suggest_field,omitempty"`
+}
 
 func main() {
 	// Obtain a client for an Elasticsearch cluster of two nodes,
@@ -30,5 +43,64 @@ func main() {
 	fmt.Println("client start:", client)
 	if err != nil {
 		fmt.Println(err)
+	}
+
+	// Create a new index.
+	indexName := "twitter"
+	ctx := context.Background()
+	createIndex, err := client.CreateIndex(indexName).Do(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if !createIndex.Acknowledged {
+		fmt.Println("create index not acknowledged")
+	}
+
+	// Index a tweet (using JSON serialization)
+	tweet1 := Tweet{User: "olivere", Message: "Take Five", Retweets: 0}
+	_, err = client.Index().
+		Index(indexName). // es index name
+		Type("tweet").
+		Id("1").
+		BodyJson(tweet1).
+		Do(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Search with a term query
+	searchResult, err := client.Search().
+		Index(indexName). // search in index t.index
+		Query(elastic.NewTermQuery("user", "olivere")). // specify the query
+		Sort("user", true). // sort by "user" field, ascending
+		From(0).Size(10). // take documents 0-9
+		Pretty(true). // pretty print request and response JSON
+		Do(ctx) // execute
+	if err != nil {
+		fmt.Printf("Search failed: %v\n", err)
+		return
+	}
+	// Number of hits
+	if searchResult.Hits.TotalHits > 0 {
+		fmt.Printf("Found a total of %d tweets\n", searchResult.Hits.TotalHits)
+
+		// Iterate through results
+		for _, hit := range searchResult.Hits.Hits {
+			// hit.Index contains the name of the index
+
+			// Deserialize hit.Source into a Tweet (could also be just a map[string]interface{}).
+			var tweet Tweet
+			err := json.Unmarshal(*hit.Source, &tweet)
+			if err != nil {
+				// Deserialization failed
+				fmt.Printf("Deserialize failed: %v\n", err)
+				continue
+			}
+		}
+	} else {
+		// No hits
+		fmt.Print("Found no tweets\n")
 	}
 }
