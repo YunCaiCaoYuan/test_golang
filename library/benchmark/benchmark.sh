fab03ce299738c2f7fd6f4b6ea1444554dfac6ae
sunb <769460962@qq.com>
2022-07-16 20:38:33 +0800

    脱敏

--- library/benchmark/benchmark.sh
+++ library/benchmark/benchmark.sh
@@ -2,4 +2,4 @@
 go test -v  --tags "local beta" ../../yes/channel/handler/ext_test.go -run TestChannelExtObj_FetchHomeChannels
 
 ## 执行tb
-./tb -c 10 -n 2 -I 60 -p http -D 47.106.118.73 -P 80 -u https://beta.2tianxin.com/proxymsg -F ../../yes/channel/handler/bin -H "X-Token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MzIzODkwOTgsImlzcyI6InllcyIsImFpZCI6OCwidWlkIjoxMDMxMDEyLCJvYmYiOiIxZjNlYWU4MTM0MzFmOWYifQ.wNRGMs3nReGfP7oy_vIx39IJ8QX5SjiedxIF_RvSVhI"
+./tb -c 10 -n 2 -I 60 -p http -D 47.106.118.73 -P 80 -u https://xxx/proxymsg -F ../../yes/channel/handler/bin -H "X-Token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MzIzODkwOTgsImlzcyI6InllcyIsImFpZCI6OCwidWlkIjoxMDMxMDEyLCJvYmYiOiIxZjNlYWU4MTM0MzFmOWYifQ.wNRGMs3nReGfP7oy_vIx39IJ8QX5SjiedxIF_RvSVhI"
