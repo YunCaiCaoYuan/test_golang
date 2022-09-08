9266db7bd6b163b6580df787da23e4f2c4536c9f
sunbin <yourName@gmail.com>
2022-09-08 21:05:31 +0800

    dev:有效括号

--- algorithm/string_vaild_bracket/vaild_bracket_test.go
+++ algorithm/string_vaild_bracket/vaild_bracket_test.go
@@ -1,7 +1,57 @@
 package string_vaild_bracket
 
+import (
+	"fmt"
+	"testing"
+)
+
 // 有效的括号
 // [] {} ()
 // 栈
 
+var Stack = make([]byte, 0)
 
+func PushStack(c byte) {
+	Stack = append(Stack, c)
+}
+func IsStackEmpty() bool {
+	return len(Stack) == 0
+}
+func PopStack() {
+	if len(Stack) == 1 {
+		Stack = make([]byte, 0)
+		return
+	}
+	Stack = Stack[:len(Stack)-2]
+}
+func TopStack() byte {
+	return Stack[len(Stack)-1]
+}
+
+func ValidBracket(str []byte) bool {
+	for _, c := range str {
+		if c == '{' || c == '[' || c == '(' {
+			PushStack(c)
+		}
+		if c == '}' || c == ']' || c == ')' {
+			if !IsStackEmpty() {
+				t := TopStack()
+				if c == '}' && t == '{' || c == ']' && t == '[' || c == ')' && t == 'c' {
+					PopStack()
+					continue
+				}
+				return false // 右出现了，左边不匹配
+			}
+			return false //右出现了，空了
+		}
+	}
+	return len(Stack) == 0 // 判断结束了,未消耗完
+}
+
+func Test_ValidBracket(t *testing.T) {
+	str := []byte{'{', '}'}
+	fmt.Println(str, " isValid: ", ValidBracket(str))
+
+	str = []byte{'}', '{'}
+	fmt.Println(str, " isValid: ", ValidBracket(str))
+}
