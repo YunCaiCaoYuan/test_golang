b21c082570166602f8ac11290d4d5c553f199a4e
孙斌 <sunbin@xinyu668.com>
2022-12-21 20:32:09 +0800

    处理post请求

--- library/test/answer_server/main.go
+++ library/test/answer_server/main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func handleCommitAnswer(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("path:", r.URL.Path, "method:", r.Method)
+	fmt.Println("path:", r.URL.Path, "method:", r.Method, "form:", r.Form, "PostForm:", r.PostForm, "header:", r.Header)
 	if r.Method == "GET" {
 		w.Write([]byte("hello1"))
 		return
 	}
 
 	if r.Method == "POST" {
+		b, _ := ioutil.ReadAll(r.Body)
+		fmt.Println("len(b):", len(b), "cap(b):", cap(b))
+		fmt.Println("body:", string(b))
+
+		//fmt.Println("r.Form.Get(passwd)", r.Form.Get("passwd"))
+
+		//decoder := json.NewDecoder(r.Body)
+		//var params map[string]string
+		//decoder.Decode(&params)
+		//fmt.Println("len(params):", len(params))
+		//for k, v := range params {
+		//	fmt.Println("k:", k, "v:", v)
+		//}
+
 		w.Write([]byte("hello2"))
 		return
 	}
